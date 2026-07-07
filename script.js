a9cadda609960daf7c4d04f67b531206ad036706
Liam_Araya <liamwork999@gmail.com>
2026-07-07 17:12:26 +0300

    Starting CSS DOM manuplation

--- script.js
+++ script.js
@@ -2,9 +2,38 @@
 
 'use strict';
 
-const score = 5;
-const hiddenNumber = 13;
-// document.querySelector('.guess').textContent = hiddenNumber;
-// document.querySelector('.score').textContent = score;
-document.querySelector('.input-number').value = 0;
-// console.log(document.querySelector('.input-number').value);
+const hiddenNumber = Math.trunc(Math.random() * 20) + 1;
+
+let score = 20;
+document.querySelector('.score-label .score').textContent = score;
+
+document.querySelector('.check-box').addEventListener('click', function () {
+  const guessed = Number(document.querySelector('.input-number').value);
+
+  if (!guessed || guessed < 1 || guessed > 20) {
+    document.querySelector('.message').textContent = '🚫Not Valid!🚫';
+  } else if (guessed === hiddenNumber) {
+    document.querySelector('.message').textContent = '🎉You Win🎉';
+    document.querySelector('.guess').textContent = String(hiddenNumber);
+  } else if (guessed > hiddenNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '☝️Too High☝️';
+      score--;
+      document.querySelector('.score-label .score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥You Lose💥';
+      document.querySelector('.score-label .score').textContent = 0;
+    }
+  } else if (guessed < hiddenNumber) {
+    if (score > 1) {
+      document.querySelector('.message').textContent = '👇Too Low👇';
+      score--;
+      document.querySelector('.score-label .score').textContent = score;
+    } else {
+      document.querySelector('.message').textContent = '💥You Lose💥';
+      document.querySelector('.score-label .score').textContent = 0;
+    }
+  }
+});
+
+console.log(hiddenNumber);
